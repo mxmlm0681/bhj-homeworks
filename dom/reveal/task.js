4b9d7a04b189e7ad1dd12f8cecec5b895778f274
maksimmarchenko <max-81mail.ru@mail.ru>
2024-10-22 06:20:13 +0300

    first code

---
+++ dom/reveal/task.js
@@ -0,0 +1,28 @@
+let listitems = document.querySelectorAll('.reveal');
+
+const isInViewport = function (element) {
+    const viewportHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const elementHeight = element.getBoundingClientRect().height;
+    return elementTop < viewportHeight && elementTop > -elementHeight ?
+        true:
+        false;
+};
+
+function checkReveals() {
+    [...listitems].forEach(element => {
+        if (isInViewport(element)) {
+            if (!element.classList.contains('reveal_active')) {
+                element.classList.add('reveal_active');
+            }
+        } else {
+            if (element.classList.contains('reveal_active')) {
+                element.classList.remove('reveal_active');
+            }
+        }
+    });
+} 
+
+window.addEventListener('scroll', checkReveals, false);
+
+checkReveals();
