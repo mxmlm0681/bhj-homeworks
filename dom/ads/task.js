29b3ad727430d2043e9f3ac55973943a5e3947ef
maksimmarchenko <max-81mail.ru@mail.ru>
2024-10-22 06:40:00 +0300

    first code

---
+++ dom/ads/task.js
@@ -0,0 +1,21 @@
+const banners = document.getElementsByClassName('rotator');
+
+[...banners].forEach(banner => {
+    const bannerList = banner.getElementsByClassName('rotator__case');
+
+    const tick = index => {
+        [...bannerList].forEach(element => {
+            element.classList.remove('rotator__case_activate');
+        });
+
+        index = bannerList[index].nextElementSibling ? index + 1 : 0;
+
+        let speed = bannerList[index].getAttribute('data-speed');
+        let color = bannerList[index].getAttribute('dasta-color');
+
+        bannerList[index].setAttribute('style', `color: ${color}`);
+        bannerList[index].classList.add('rotator__case_active');
+    };
+
+    setTimeout(tick, 0, 0);
+});
