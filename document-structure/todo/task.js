a74bf37ab87ef552d30ccec538b5f12ffb59f74d
maksimmarchenko <max-81mail.ru@mail.ru>
2024-10-26 18:02:51 +0300

    first code

---
+++ document-structure/todo/task.js
@@ -0,0 +1,28 @@
+let inputBox = document.getElementById("task__input");
+let tasksList = document.getElementById("tasks__list");
+
+const removeTask = e => {
+    e.target.closest(".task").remove();
+}
+
+const addTask = e => {
+    tasksList.innerHTML += `<div class="task">
+        <div clas="task__title">
+            ${inputBox.value}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+    </div>`;
+
+    inputBox.value = "";
+
+    [...(tasksList.getElementsByClassName("task__remove"))].forEach(element => {
+        element.addEventListener("click", removeTask)
+    });
+
+    e.preventDefault();
+}
+
+document.getElementById("tasks__add").addEventListener("click", addTask);
+inputBox.addEventListener("keypress", e => {
+    if (e.keyCode == 13) addTask(e);
+});
